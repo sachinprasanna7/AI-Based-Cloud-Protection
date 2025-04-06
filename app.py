42f1de524182fa7e2e6782b2d3b3567f4f61217f
sachinprasanna7 <sachinprasanna.211it058@nitk.edu.in>
2025-04-06 16:42:13 +0530

    changed the flask run part to run on aws

--- app.py
+++ app.py
@@ -39,6 +39,6 @@ def predict_attack():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+    
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
